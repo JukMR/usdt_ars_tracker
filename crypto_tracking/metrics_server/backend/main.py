cc37fa6a4fa9d4aa76f69c536e0b88c7d76b43f7
Julian Merida <julianmr97@gmail.com>
2024-08-12 21:15:30 -0300

    Create folder metrics_server

--- crypto_tracking/metrics_server/backend/main.py
+++ crypto_tracking/metrics_server/backend/main.py
@@ -8,9 +8,7 @@ app = Flask(__name__)
 def handle_numbers():
     data = request.get_json()
     print("received post")
-    import pdb
 
-    pdb.set_trace()
     min_num = data["min_num"]
     max_num = data["max_num"]
     # Process the numbers here (e.g., store them in a database, perform calculations, etc.)
--- crypto_tracking/metrics_server/frontend/app.py
+++ crypto_tracking/metrics_server/frontend/app.py
@@ -1,7 +1,9 @@
-from flask import Flask, render_template, request
 import requests
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+localhost_url: str = "http/localhost/api/numbers:5001"
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -10,9 +12,9 @@ def home():
         min_num = request.form["min_num"]
         max_num = request.form["max_num"]
         # Send the numbers to the backend server
-        response = requests.post("http://localhost/api/numbers:5001", json={"min_num": min_num, "max_num": max_num})
-        return "Numbers sent successfully!"
+        response = requests.post(localhost_url, json={"min_num": min_num, "max_num": max_num}, timeout=60)
         response.raise_for_status()
+        return "Numbers sent successfully!"
     return render_template("index.html")
 
 
