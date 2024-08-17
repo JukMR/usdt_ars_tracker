7357866e3be5ac1e259daa858d72ffa86b5952db
Julian Merida <julianmr97@gmail.com>
2024-08-16 23:36:02 -0300

    Fix code quality in poller

--- crypto_tracking/api_poller/poller.py
+++ crypto_tracking/api_poller/poller.py
@@ -14,19 +14,9 @@ from crypto_tracking.metrics_server.backend.database.database_session import Dat
 from crypto_tracking.metrics_server.backend.database.sql_models import Entry
 
 
-def fetch_exchange_rate(polling_rate: int) -> dict[str, Any]:
-    """Fetch the latest exchange rate from the API."""
-    url = "https://criptoya.com/api/usdt/ars"
-    timeout: int = int(polling_rate * 0.8)  # 80% of the polling rate
-    response = requests.get(url, timeout=timeout)
-    response.raise_for_status()
-    data = response.json()
-
-    return data.get("buenbit", {})
-
-
 def poller(project_folder: Path, db_engine: Engine, polling_rate: int = 60) -> NoReturn:
-    job_instance: _Storer = _Storer(polling_rate=polling_rate, project_folder=project_folder, db_engine=db_engine)
+    """Poller function that fetches the exchange rate and stores it in the database."""
+    job_instance: JobWorker = JobWorker(polling_rate=polling_rate, project_folder=project_folder, db_engine=db_engine)
 
     schedule.every(polling_rate).seconds.do(job_instance.job)
 
@@ -43,7 +33,9 @@ def poller(project_folder: Path, db_engine: Engine, polling_rate: int = 60) -> N
             time.sleep(5)
 
 
-class _Storer:
+class JobWorker:
+    """Class that fetches the prices of the USDT from buenbit and store it in the sqlite db"""
+
     def __init__(self, polling_rate: int, project_folder: Path, db_engine: Engine) -> None:
         self.polling_rate: int = polling_rate
         self.project_folder: Path = project_folder
@@ -53,8 +45,9 @@ class _Storer:
     def job(
         self,
     ) -> None:
+        """Fetch the exchange rate and store it in the database."""
         logger.info("Fetching exchange rate...")
-        rate_data: dict[str, Any] = fetch_exchange_rate(polling_rate=self.polling_rate)
+        rate_data: dict[str, Any] = self._fetch_exchange_rate(polling_rate=self.polling_rate)
         if rate_data:
             buy = float(rate_data["totalAsk"])
             sell = float(rate_data["totalBid"])
@@ -66,16 +59,29 @@ class _Storer:
             logger.info("Stored new buy: %s at %s", rate_data["totalAsk"], current_time)
             logger.info("Stored new sell: %s at %s", rate_data["totalBid"], current_time)
 
-    def store(self, source: str, buy: float, sell: float, current_time: datetime) -> None:
-        self.insert_entry_in_database(current_time=current_time, source=source, buy=buy, sell=sell)
+    @staticmethod
+    def _fetch_exchange_rate(polling_rate: int) -> dict[str, Any]:
+        """Fetch the latest exchange rate from the API."""
+        url = "https://criptoya.com/api/usdt/ars"
+        timeout: int = int(polling_rate * 0.8)  # 80% of the polling rate
+        response = requests.get(url, timeout=timeout)
+        response.raise_for_status()
+        data = response.json()
 
-    def insert_entry_in_database(self, current_time: datetime, source: str, buy: float, sell: float) -> None:
+        return data.get("buenbit", {})
+
+    def store(self, source: str, buy: float, sell: float, current_time: datetime) -> None:
+        """Store the exchange rate somewhere."""
+        self._insert_entry_in_database(current_time=current_time, source=source, buy=buy, sell=sell)
+
+    def _insert_entry_in_database(self, current_time: datetime, source: str, buy: float, sell: float) -> None:
         """Insert the fetched exchange rate into a database."""
         with DatabaseSession(engine=self.database_engine) as db_service:
             db_service.add(Entry(datetime=current_time, source=source, buy=buy, sell=sell))
 
 
 def main() -> None:
+    """Main function that starts the poller service"""
     project_folder: Path = Path(__file__).resolve().parent.parent
     assert project_folder.name == "crypto_tracking", "Project folder is not named 'crypto_tracking'"
 
